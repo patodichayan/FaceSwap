34d879e2b290c4a5e58b22f1147accd16e66836e
Saket <ghsaketseshadri@gmail.com>
2020-03-07 00:06:09 -0500

    added get_facial_landmarks func

--- Phase1/Code/facial_landmark_det.py
+++ Phase1/Code/facial_landmark_det.py
@@ -1,1 +1,65 @@
-print("dlib")
+import cv2
+import dlib
+import numpy as np
+import copy
+
+
+def get_facial_landmarks(img=None):
+    
+    if(img==None):
+        # load image
+        file_path = '../../Data/'
+        cap = cv2.VideoCapture(file_path+'saket3.mp4')
+        # while(True):
+        ret, img = cap.read()
+        # cv2.imshow('sak', img)
+        #     cv2.waitKey(2)
+        #     if cv2.waitKey(1) & 0xFF == ord('q'):
+        #         break
+        # cv2.waitKey(0)
+        # cap.release()
+        # cv2.destroyAllWindows()
+        
+    # load shape predictor model
+    model_path = 'dlib_model/shape_predictor_68_face_landmarks.dat'
+
+    # load the detector and the predictor. 
+    # predictor accepts pre-trained model as input
+    detector = dlib.get_frontal_face_detector()
+    predictor = dlib.shape_predictor(model_path)
+
+    img_ = copy.deepcopy(img)
+    img_gray = cv2.cvtColor(img_, cv2.COLOR_BGR2GRAY)
+    rects = detector(img_gray, 1)
+
+    for i, rect in enumerate(rects):
+        landmarks = predictor(img_gray, rect)
+
+        # reshape landmarks to (68X2)
+        landmark_coord = np.zeros((68, 2), dtype='int')
+
+        for i in range(68):
+            landmark_coord[i] = (landmarks.part(i).x, landmarks.part(i).y)
+
+        # draw bounding box on face
+        #cv2.rectangle(img_, (rect.left(), rect.top()), (rect.right(), rect.bottom()), (0, 255, 255), 4)
+
+        # draw facial landmarks
+        
+        jaw = landmark_coord[0:17]
+        left_ebrow = landmark_coord[17:22]
+        right_ebrow = landmark_coord[22:27]
+        nose = landmark_coord[27:36]
+        eye_left = landmark_coord[36:42]
+        eye_right = landmark_coord[42:48]
+        lips = landmark_coord[48:]
+
+        cv2.polylines(img_, [jaw], False, (0, 255, 0), 1)
+        cv2.polylines(img_, [left_ebrow], False, (0, 255, 0), 1)
+        cv2.polylines(img_, [right_ebrow], False, (0, 255, 0), 1)
+        cv2.polylines(img_, [nose], False, (0, 255, 0), 1)
+        cv2.polylines(img_, [eye_left], False, (0, 255, 0), 1)
+        cv2.polylines(img_, [eye_right], False, (0, 255, 0), 1)
+        cv2.polylines(img_, [lips], False, (0, 255, 0), 1)
+
+    return img_
