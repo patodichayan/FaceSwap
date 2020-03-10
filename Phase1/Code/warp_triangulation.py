7b567d4b087e9e8de8542a965fa40551045c8f22
Saket <ghsaketseshadri@gmail.com>
2020-03-09 23:31:58 -0400

    done triangulation on both faces

--- Phase1/Code/warp_triangulation.py
+++ Phase1/Code/warp_triangulation.py
@@ -1,1 +1,70 @@
-print("triangulation")
+import cv2
+# import dlib
+import numpy as np
+import copy
+import random
+
+def draw_triangles(img, triangleList, face2=False, landmark_coord=None):
+    
+    # for getting random colors for plotting triangles
+    colors = []
+    color_ind = [random.choice(range(-90, 40)) for i in range(70)]
+    
+    blue_shades = [(150+c, 0, 0) for c in color_ind]
+    green_shades = [(0, 175+c, 0) for c in color_ind]
+    red_shades = [(0, 0, 140+c) for c in color_ind]
+
+    random.shuffle(blue_shades)
+    random.shuffle(red_shades)
+    random.shuffle(green_shades)
+    for b, g, r in zip(blue_shades, green_shades, red_shades):
+        colors.append((b[0], g[1], r[2]))
+    
+    # define a list to store the index of which points are used in triangle formation
+    triangleIdList = []
+    for t in triangleList:
+        
+        # get the three vertices of each triangle to show
+        pt1 = (t[0], t[1])
+        pt2 = (t[2], t[3])
+        pt3 = (t[4], t[5])
+        
+        color = random.choice(colors)
+        cv2.line(img, pt1, pt2, color, 1, cv2.LINE_AA, 0)
+        cv2.line(img, pt2, pt3, color, 1, cv2.LINE_AA, 0)
+        cv2.line(img, pt3, pt1, color, 1, cv2.LINE_AA, 0)
+        
+        if(not face2):
+            # now find out where these points occur so that we can draw corresponding
+            # triangles in second face
+            pt1_id = np.where((landmark_coord==pt1).all(axis=1))
+            pt2_id = np.where((landmark_coord==pt2).all(axis=1))
+            pt3_id = np.where((landmark_coord==pt3).all(axis=1))
+
+            triangleIdList.append([pt1_id, pt2_id, pt3_id])
+    
+    if(not face2):
+        return img, triangleIdList
+    else:
+        return img
+
+def triangulation(img, landmark_coord):
+    
+    # get the bounding rectangle of the landmarks
+    h, w = img.shape[0], img.shape[1]
+    rect = (0, 0, w, h)
+    
+    # Create an instance of Subdiv2D with the rectangle 
+    # obtained in the previous step
+    subdiv = cv2.Subdiv2D(rect)
+
+    # insert the points into subdiv             
+    for coord in landmark_coord:
+        subdiv.insert((coord[0], coord[1]))
+        
+    # draw the delaunay triangles
+    triangleList = subdiv.getTriangleList()
+    
+    img, triangleIdList = draw_triangles(img, triangleList, False, landmark_coord)
+    
+    return img, triangleIdList
--- Phase1/Code/wrapper.py
+++ Phase1/Code/wrapper.py
@@ -1,2 +1,48 @@
-print("wrapper")
-print("dgfdgfd)
+import cv2
+import numpy as np
+import copy
+import random
+
+from warp_triangulation import draw_triangles, triangulation
+from facial_landmark_det import get_facial_landmarks
+
+def main():
+
+	file_path = '../../Data/'
+	cap = cv2.VideoCapture(file_path+'together5.mp4')
+	# while(True):
+	ret, img = cap.read()
+	img_copy = copy.deepcopy(img)
+
+	# load the image and calculate facial landmarks using dlib
+	face_ldmrk_img, landmark_coord_all = get_facial_landmarks(img)
+
+	# display the facial landmarks
+	cv2.imshow("OP_face_ldmrk", face_ldmrk_img)
+
+	# perform triangulation on 1st face and gather their locations in the landmark_coord array
+	triang_fc1_img, triangleIdList = triangulation(img_copy, landmark_coord_all[0])
+
+	# show the output image with the delaunay triangulation on face 1
+	cv2.imshow("OP_delaunay_fc1", triang_fc1_img)
+
+	# retrieve the locations of the triangles in the second face
+	triangleList = []
+	for t in triangleIdList:
+	    p1_id, p2_id, p3_id = t[0], t[1], t[2]
+	    pt1 = landmark_coord_all[1][p1_id][0]
+	    pt2 = landmark_coord_all[1][p2_id][0]
+	    pt3 = landmark_coord_all[1][p3_id][0]
+
+	    triangleList.append([pt1[0], pt1[1], pt2[0], pt2[1], pt3[0], pt3[1]])
+
+	# draw triangles on face2
+	triang_fc2_img = draw_triangles(triang_fc1_img, triangleList, face2=True, landmark_coord=None)
+
+	# show the output image with the delaunay triangulation
+	cv2.imshow("OP_delaunay_fc12", triang_fc2_img)
+	cv2.waitKey(0)
+	cv2.destroyAllWindows()
+
+if __name__ == '__main__':
+	main()
